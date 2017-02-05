4f41e51441c401314fab4968a9202e9bdf3dde2d
Austin <acm.liu11@gmail.com>
2017-02-04 22:53:59 -0800

    configure mongodb

--- client/main.jsx
+++ client/main.jsx
@@ -4,6 +4,8 @@ import { render } from 'react-dom';
  
 import App from '../imports/ui/App.jsx';
  
+//entry point loaded on the client
+
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
 });
